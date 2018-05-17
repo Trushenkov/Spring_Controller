911b79a0ed6201338f07318a8f437ed4f567b9c6
Trushenkov Dmitry <diman28051999@gmail.com>
2018-05-17 19:12:07 +0300

    MainTests - updated. Added 1 test for method request()

--- src/test/java/com/example/demo/MainTests.java
+++ src/test/java/com/example/demo/MainTests.java
@@ -5,14 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainTests {
 
     @Test
     public void contextLoads() {
+        SimpleController controller = new SimpleController();
+        controller.request();
 
-
+        assertEquals("GET запрос успешно выполнен.",controller.request());
     }
 
 }
