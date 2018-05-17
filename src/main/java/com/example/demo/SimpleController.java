ff7a77c5dda945f61db70fd23795037c115bd72d
Trushenkov Dmitry <diman28051999@gmail.com>
2018-05-17 21:18:45 +0300

    Added one more request

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -8,11 +8,18 @@ public class SimpleController {
 
     private static final String string = "/request";
 
+    private static final String welcome = "/welcome";
+
     @RequestMapping(value = string)
     public String request() {
         return "GET запрос успешно выполнен.";
     }
 
+
+    @RequestMapping(value = welcome)
+    public String welcome() {
+        return "Welcome on my page, User!";
+    }
 
 
 }
